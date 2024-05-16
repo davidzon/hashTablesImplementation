61ace86688894f4fdb1a5e3a438bb6fd2db1353f
shakgill786 <sgillani1@gmail.com>
2024-05-16 19:18:12 -0400

    resize

--- phases/01-implementation.js
+++ phases/01-implementation.js
@@ -9,8 +9,9 @@ class KeyValuePair {
 class HashTable { // get O(1), set O(1), deleteKey O(1)
 
   constructor(numBuckets = 8) {
-    // Initialize your buckets here
-    // Your code here 
+    this.capacity = numBuckets;
+    this.data = new Array(numBuckets).fill(null);
+    this.count = 0;
   }
 
   hash(key) {
@@ -30,18 +31,75 @@ class HashTable { // get O(1), set O(1), deleteKey O(1)
 
 
   insert(key, value) {
-    // Your code here 
-  }
+    const index = this.hashMod(key);
+    const newNode = new KeyValuePair(key, value);
 
+    if (this.data[index] === null){
+      this.data[index] = newNode;
+    } else {
+      let current = this.data[index];
+
+      while (current !== null){
+        if (current.key === key){
+          current.value = value;
+          return;
+        }
+        current = current.next;
+      }     
+
+          newNode.next = this.data[index];
+          this.data[index] = newNode;
+      }
+      
+        this.count ++;
+      }
 
   read(key) {
-    // Your code here 
+    const index = this.hashMod(key);
+    let current = this.data[index];
+
+    while (current) {
+      if (current.key === key){
+        return current.value;
+      }
+      current = current.next;
+    }
+    return undefined; 
   }
 
 
   resize() {
-    // Your code here 
-  }
+    const oldData = this.data;
+    const oldCapacity = this.capacity;
+
+    this.capacity *= 2;
+    this.data = new Array(this.capacity).fill(null);
+    this.count = 0;
+
+    oldData.forEach(bucket => {
+      let current = bucket;
+
+      while (current){
+          const index = this.hashMod(current.key);
+          const newNode = new KeyValuePair(current.key, current.value);
+
+          if (!this.data[index]){
+            this.data[index] = newNode;
+          } else {
+            let existingNode = this.data[index];
+
+            while (existingNode.next){
+              existingNode = existingNode.next;
+            }
+            existingNode.next = newNode;
+          }
+
+          this.count++;
+          current = current.next;
+          }
+      });
+    }
+
 
 
   delete(key) {
